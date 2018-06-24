bcace4d0734aba50339937144d5f8e441d46aa25
Gina Lee <ginalee1114@gmail.com>
2018-06-24 01:58:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,4 +7,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist;
 }
 
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
 
